83465966353535e169b48cd76fdd36ac3413b62f
Lucas Robitaille <lrjames237@gmail.com>
2019-10-15 16:15:00 -0500

    added takesService functions

---
+++ src/takes/takes-service.js
@@ -0,0 +1,24 @@
+const TakesService = {
+    getAllTakes(knex) {
+      return knex.select('*').from('takes')
+    },
+    insertTake(knex, newTake) {
+      return knex
+        .insert(newTake)
+        .into('takes')
+        .returning('*')
+        .then(rows => {
+          return rows[0]
+        })
+    },
+    getById(knex, id) {
+      return knex.from('takes').select('*').where('id', id).first()
+    },
+    deleteTake(knex, id) {
+      return knex('takes')
+        .where({ id })
+        .delete()
+    },
+  }
+  
+  module.exports = TakesService
