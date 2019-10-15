cdece2bf09f9f595fbe1bad82b15e536031f9b52
Lucas Robitaille <lrjames237@gmail.com>
2019-10-15 15:01:14 -0500

    created commentators router get post delete

---
+++ src/commentators/commentators-router.js
@@ -0,0 +1,82 @@
+const path = require('path')
+const express = require('express')
+const CommentatorService = require('./commentators-service')
+
+const commentatorsRouter = express.Router()
+const jsonParser = express.json()
+
+const serializeCommentator = commentator => ({
+    id: commentator.id,
+    name: commentator.name,
+    network: commentator.network,
+    twitter: commentator.twitter,
+    instagram: commentator.instagram,
+    about: commentator.about,
+})
+
+commentatorsRouter
+    .route('/')
+    .get((req, res, next) => {
+        const knexInstance = req.app.get('db')
+        CommentatorService.getAllCommentators(knexInstance)
+            .then(commentators => {
+                res.json(commentators.map(serializeCommentator))
+            })
+            .catch(next)
+    })
+    .post(jsonParser, (req, res, next) => {
+        const { name, network } = req.body
+        const newCommentator = { name, network }
+
+        for (const [key, value] of Object.entries(newCommentator))
+            if (value == null)
+                return res.status(400).json({
+                error: { message: `Missing '${key}' in request body` }
+                })
+            CommentatorService.insertCommentator(
+            req.app.get('db'),
+            newCommentator
+            )
+            .then(commentator => {
+                res
+                .status(201)
+                .location(path.posix.join(req.originalUrl, `/${commentator.id}`))
+                .json(serializeCommentator(commentator))
+            })
+            .catch(next)
+    })
+
+
+commentatorsRouter
+    .route('/:commentatorId')
+    .get((req, res, next) => {
+        CommentatorService.getById(
+            req.app.get('db'),
+            req.params.commentatorId
+        )
+        .then(commentator => {
+            if(!commentator) {
+                return res.status(404).json({
+                    error: { message: `Commentator doesn't exist`}
+                })
+            }
+            res.commentator = commentator
+            next()
+        })
+        .catch(next)
+    })
+    .get((req, res, next) => {
+        res.json(serializeCommentator(res.commentator))
+    })
+    .delete((req, res, next) => {
+        CommentatorService.deleteCommentator(
+            req.app.get('db'),
+            req.params.commentatorId
+        )
+        .then(numRowsAffected => {
+            res.status(204).end()
+        })
+        .catch(next)
+    })
+
+module.exports = commentatorsRouter
---
+++ src/commentators/commentators-service.js
@@ -0,0 +1,24 @@
+const CommentatorsService = {
+    getAllCommentators(knex) {
+        return knex.select('*').from('commentators')
+    },
+    insertCommentator(knex, newCommentator) {
+        return knex
+            .insert(newCommentator)
+            .into('commentators')
+            .returning('*')
+            .then(rows => {
+            return rows[0]
+            })
+    },
+    getById(knex, id) {
+        return knex.from('commentators').select('*').where('id', id).first()
+    },
+    deleteCommentator(knex, id) {
+        return knex('commentators')
+            .where({ id })
+            .delete()
+    },
+}
+
+module.exports = CommentatorsService
