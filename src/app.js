c84e923838aefb11d2181e5afb541307873e318b
Lucas Robitaille <lrjames237@gmail.com>
2019-10-15 10:12:43 -0500

    added api test endpoint

--- src/app.js
+++ src/app.js
@@ -19,15 +19,10 @@ app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
 
-app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
+app.get('/api/*', (req, res) => {
+    res.json({ok: true});
 })
+
+
 
 module.exports = app
