91d44e015b127f7d6d63c655c4c75dc9cec2a4b7
Lucas Robitaille <lrjames237@gmail.com>
2019-10-15 11:07:44 -0500

    added error handler function

--- src/app.js
+++ src/app.js
@@ -23,6 +23,17 @@ app.get('/api/*', (req, res) => {
     res.json({ok: true});
 })
 
+app.use(function errorHandler(error, req, res, next) {
+  let response
+  if (NODE_ENV === 'production') {
+      response = { error: { message: 'server error' } }
+  } else {
+      console.error(error)
+      response = { message: error.message, error }
+  }
+  res.status(500).json(response)
+})
+
 
 
 module.exports = app
