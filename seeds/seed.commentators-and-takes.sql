0336055ecf60f467630f9a75bda4a3e82c6730dd
Lucas Robitaille <lrjames237@gmail.com>
2019-10-17 10:13:25 -0500

    changed seed file

--- seeds/seed.commentators-and-takes.sql
+++ seeds/seed.commentators-and-takes.sql
@@ -4,15 +4,15 @@ INSERT INTO commentators
     (name, network, twitter, instagram, about)
 VALUES
   ('Bill Simmons', 'The Ringer', 'https://twitter.com/BillSimmons', 'https://www.instagram.com/sptguy33/', 'William John Simmons III, is an American former sports writer and current sports analyst, author, and podcaster. Simmons first gained attention with his website as "The Boston Sports Guy" and was recruited by ESPN in 2001, where he eventually operated the website Grantland and worked until 2015.'),
-  ('Dave Portnoy', 'Dave Portnoy', 'https://twitter.com/stoolpresidente', 'https://www.instagram.com/stoolpresidente/', 'David Portnoy is an American internet celebrity, blogger, and founder of the satirical sports and pop culture blog Barstool Sports.'),
+  ('Dave Portnoy', 'Barstool Sports', 'https://twitter.com/stoolpresidente', 'https://www.instagram.com/stoolpresidente/', 'David Portnoy is an American internet celebrity, blogger, and founder of the satirical sports and pop culture blog Barstool Sports.'),
   ('Stephen A. Smith', 'ESPN', 'https://twitter.com/stephenasmith', 'https://www.instagram.com/stephenasmith/', 'Stephen Anthony Smith is an American sports television personality, sports radio host, sports journalist, and actor. Smith is a commentator on ESPN First Take, where he appears with Max Kellerman and Molly Qerim. He also makes frequent appearances as an NBA analyst on SportsCenter');
 
 INSERT INTO takes (take, date, commentatorId, commentator, correct, sport) VALUES
-  ('The Celtics will beat the Cavs by 50 points', '10/13/19', '1', 'Bill Simmons', FALSE, 'NBA'),
-  ('The Chiefs will beat the Texans.', '10/13/19', '1', 'Bill Simmons', FALSE, 'NFL'),
-  ('The Patriots will beat the Giants', '10/10/19', '2', 'Dave Portnoy', TRUE, 'NFL'),
-  ('Kyrie Irving will leave the Celtics', '07/31/19', '1', 'Bill Simmons', TRUE, 'NBA'),
-  ('The Raptors will beat the Warriors in the Finals', '06/01/19', '1', 'Bill Simmons"', TRUE, 'NBA'),
-  ('Patrick Mahomes will win the MVP for the 2018 season', '08/01/18', '3', 'Stephen A. Smith', TRUE, 'NFL');
+  ('The Celtics will beat the Cavs by 50 points', '10/13/19', '1', 'Bill Simmons', 'FALSE', 'NBA'),
+  ('The Chiefs will beat the Texans.', '10/13/19', '1', 'Bill Simmons', 'FALSE', 'NFL'),
+  ('The Patriots will beat the Giants', '10/10/19', '2', 'Dave Portnoy', 'TRUE', 'NFL'),
+  ('Kyrie Irving will leave the Celtics', '07/31/19', '1', 'Bill Simmons', 'TRUE', 'NBA'),
+  ('The Raptors will beat the Warriors in the Finals', '06/01/19', '1', 'Bill Simmons', 'TRUE', 'NBA'),
+  ('Patrick Mahomes will win the MVP for the 2018 season', '08/01/18', '3', 'Stephen A. Smith', 'TRUE', 'NFL');
 
 COMMIT;
