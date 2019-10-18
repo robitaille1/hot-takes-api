09cb76aac812a93531eadb3b9a2d243045c6e97d
Lucas Robitaille <lrjames237@gmail.com>
2019-10-18 10:56:14 -0500

    added commentators endpoint tests

--- test/commentators-endpoints.spec.js
+++ test/commentators-endpoints.spec.js
@@ -1,15 +1,12 @@
 const knex = require('knex')
 const app = require('../src/app')
+const fixtures = require('./commentators-fixtures')
 const helpers = require('./test-helpers')
+
 
 describe('Commentators Endpoints', function() {
     let db
     
-    const {
-        testCommentators,
-        testTakes
-    } = helpers.makeCommentatorsFixtures()
-
     before('make knex instance', () => {
         db = knex({
           client: 'pg',
@@ -31,6 +28,122 @@ describe('Commentators Endpoints', function() {
               .get('/api/commentators')
               .expect(200, [])
           })
-        })      
+        })
+        context('Given there are commentators in the database', () => {
+          const testCommentators = fixtures.makeCommentatorsArray()
+    
+          beforeEach('insert Commentators', () => {
+            return db
+              .into('commentators')
+              .insert(testCommentators)
+          })
+    
+          it('gets the commentators from the database', () => {
+            return supertest(app)
+              .get('/api/commentators')
+              .expect(200, testCommentators)
+          })
+        })
+    })
+    describe('GET /api/commentators/:id', () => {
+      context(`Given no commentators`, () => {
+        it(`responds 404 whe commentator doesn't exist`, () => {
+          return supertest(app)
+            .get(`/api/commentators/123`)
+            .expect(404, {
+              error: { message: `Commentator doesn't exist` }
+            })
+        })
+      })
+      context('Given there are commentators in the database', () => {
+        const testCommentators = fixtures.makeCommentatorsArray()
+  
+        beforeEach('insert commentators', () => {
+          return db
+            .into('commentators')
+            .insert(testCommentators)
+        })
+  
+        it('responds with 200 and the specified commentator', () => {
+          const commentatorId = 2
+          const expectedCommentator = testCommentators[commentatorId - 1]
+          return supertest(app)
+            .get(`/api/commentators/${commentatorId}`)
+            .expect(200, expectedCommentator)
+        })
+      })
+    })
+    describe('DELETE /api/commentators/:id', () => {
+      context('Given there are commentators in the database', () => {
+        const testCommentators = fixtures.makeCommentatorsArray()
+  
+        beforeEach('insert commentators', () => {
+          return db
+            .into('commentators')
+            .insert(testCommentators)
+        })
+  
+        it('removes the commentator by ID from the store', () => {
+          const idToRemove = 2
+          const expectedCommentators = testCommentators.filter(cm => cm.id !== idToRemove)
+          return supertest(app)
+            .delete(`/api/commentators/${idToRemove}`)
+            .expect(204)
+            .then(() =>
+              supertest(app)
+                .get(`/api/commentators`)
+                .expect(expectedCommentators)
+            )
+        })
+      })
+    })
+    describe('POST /api/commentators', () => {
+      ['network', 'twitter', 'instagram', 'about'].forEach(field => {
+        const newCommentator = {
+          name: 'test-name',
+          network: 'TEST',
+          twitter: 'https://test.com',
+          instagram: 'https://test.com',
+          about: 'I am a test commentator'
+        }
+  
+        it(`responds with 400 missing '${field}' if not supplied`, () => {
+          delete newCommentator[field]
+  
+          return supertest(app)
+            .post(`/api/commentators`)
+            .send(newCommentator)
+            .expect(400, {
+              error: { message: `Missing '${field}' in request body` }
+            })
+        })
+      })
+      it('adds a new commentator to the database', () => {
+        const newCommentator = {
+          name: 'test-name',
+          network: 'TEST',
+          twitter: 'https://test.com',
+          instagram: 'https://test.com',
+          about: 'I am a test commentator'
+        }
+        return supertest(app)
+          .post(`/api/commentators`)
+          .send(newCommentator)
+          .expect(201)
+          .expect(res => {
+            expect(res.body.name).to.eql(newCommentator.name)
+            expect(res.body.network).to.eql(newCommentator.network)
+            expect(res.body.twitter).to.eql(newCommentator.twitter)
+            expect(res.body.instagram).to.eql(newCommentator.instagram)
+            expect(res.body.about).to.eql(newCommentator.about)
+            expect(res.body).to.have.property('id')
+            expect(res.headers.location).to.eql(`/api/commentators/${res.body.id}`)
+          })
+          .then(res =>
+            supertest(app)
+              .get(`/api/commentators/${res.body.id}`)
+              .expect(res.body)
+          )
+      })
     })
 })
---
+++ test/commentators-fixtures.js
@@ -0,0 +1,40 @@
+function makeCommentatorsArray() {
+    return [
+        {
+            id: 1,
+            name: 'Dave Test',
+            network: 'test',
+            twitter: 'https://www.twitter.com',
+            instagram: 'https://www.instagram.com',
+            about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
+        },
+        {
+            id: 2,
+            name: 'Billiam Test',
+            network: 'test',
+            twitter: 'https://www.twitter.com',
+            instagram: 'https://www.instagram.com',
+            about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
+        },
+        {
+            id: 3,
+            name: 'Jefe Test',
+            network: 'test',
+            twitter: 'https://www.twitter.com',
+            instagram: 'https://www.instagram.com',
+            about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
+        },
+        {
+            id: 4,
+            name: 'Dad Test',
+            network: 'test',
+            twitter: 'https://www.twitter.com',
+            instagram: 'https://www.instagram.com',
+            about: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
+        }
+    ]
+  }
+
+  module.exports = {
+    makeCommentatorsArray,
+  }
